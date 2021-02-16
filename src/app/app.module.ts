563363ce3ea314fe055aeb88e3dd6f3d5381ddd5
gtretow <guilherme.tretow@gmail.com>
2021-02-16 19:46:11 -0300

    testes com injeção de dependencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,9 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -27,7 +29,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ProductCreateComponent,
+    ProductCrudComponent
    
   ],
   imports: [
@@ -37,7 +39,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
